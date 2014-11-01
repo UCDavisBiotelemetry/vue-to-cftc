db5976da765c5bf75679ba9de8fe460bb21b1962
pagel <pagel@cs.wisc.edu>
2014-11-01 00:35:10 +0000

    Support for VUE 1.8 CSVs

--- ConvertVUE2forCFTC.pl
+++ ConvertVUE2forCFTC.pl
@@ -9,8 +9,8 @@
 # Modified for VUE version 2 CSV output on 20141014 Matt Pagel
 # Various error handling for atypical VRL -> CSV output added 20141024 MP
 
-# VUE 1.0 CSV output
-# DetectDate, codespace, tagID, data1, units1, data2, units2, ?7, ?8, ?9, VR2SN
+# VUE 1.0 CSV output (14 columns, 13 commas)
+# DateTime,Code Space,ID,Sensor 1,Units 1,Sensor 2,Units 2,Transmitter Name,Transmitter S/N,Receiver Name,Receiver S/N,Station Name,Station Latitude,Station Longitude
 
 # VUE 2.0 CSV output
 # Date and Time (UTC),Receiver,Transmitter,Transmitter Name,Transmitter Serial,Sensor Value,Sensor Unit,Station Name,Latitude,Longitude
@@ -26,19 +26,23 @@ my @WARNINGS;
 $SIG{__WARN__} = sub { push @WARNINGS, shift };
 
 # with "strict" we need to declare all variables prior to use
-my ($line, $junk, $year, $mon, $day, $hh, $mm, $ss, $MMDDYYYY, $HHMMSS, $outdate, $sernumstring, $sernum, $codespacetag, $tech, $spce, $tagcode, $codespace, $data1, $units1, $data2, $units2, $outstring, $datestring);
+my ($line, $junk, $year, $mon, $day, $hh, $mm, $ss, $MMDDYYYY, $HHMMSS, $outdate, $sernumstring, $sernum, $codespacetag, $tech, $spce, $tagcode, $codespace, $data1, $units1, $data2, $units2, $outstring, $datestring, $ver);
 
 # read in header line, look at number of commas to decide what the format is. For another day: handle VUE 1.0 and/or handle VUE 2.0 sans header.
-if ((<STDIN> =~ tr/,//) == 11) { croak("This perl script is written for VUE 2.0 CSV output, not VUE 1.0 CSV output"); } 
+my $coms = (<STDIN> =~ tr/,//);
+if ($coms == 13) { $ver = 1; }
+elsif ($coms == 9) { $ver = 2; }
+else { croak("Unrecognized VUE CSV format. This script written primarily for 'default' format in VUE 2.1.3"); }
+#no switch/case in Perl by default?  who knew?
 
 # work on the rest of the lines.
 while ($line = <STDIN>) {
-  $line =~ s/\r?\n|\r/,,,,,,,,,,1/g; # instead of cho(m)p for endline processing, we do this instead to remove any and all endlines of DOS or linux CSVs - the commas and the placeholder at end are to fill out incomplete lines
+  $line =~ s/\r?\n|\r/,,,,,,,,,,,,,,1/g; # instead of cho(m)p for endline processing, this removes any and all endlines of DOS or linux CSVs - the commas and the placeholder at end are to fill out incomplete lines
   my @fields = split(/,/, $line);
 
-  # BLOCK: Process the date and time
+  # BLOCK: Process the date and time, same for CSV1 and 2
   $datestring = $fields[0];
-  ($year, $mon, $day, $hh, $mm, $ss) = split(/[:\-,\s\/]/, $datestring) or carp("WARNING: error splitting $datestring");
+  ($year, $mon, $day, $hh, $mm, $ss) = split(/[:\-,\s\/]/, $datestring) or croak("ERROR: unrecognized Date-Time format in $datestring");
   if ($day > 31) { #likely MM/DD/YYYY instead of YYYY-MM-DD
 	$junk = $year; $year = $day; $day = $mon; $mon = $junk;
   }
@@ -51,22 +55,37 @@ while ($line = <STDIN>) {
   $HHMMSS = join(":", &pad($hh), &pad($mm), &pad($ss));
   $outdate = join(" ", $MMDDYYYY, $HHMMSS);
 
-  # BLOCK: Process the serial number: drop "VR2W" part - we don't need that
-  $sernumstring = $fields[1];
-  (undef, $sernum) = split(/-/, $sernumstring); 
+  # BLOCK: Process the serial number: drop "VR2W" part from CSV2 - we don't need that
+  if ($ver == 2) {
+	$sernumstring = $fields[1];
+	(undef, $sernum) = split(/-/, $sernumstring);
+  } else { #ver1
+	$sernum = $fields[10];
+  }
 
   # BLOCK: Process the codespace and tag by splitting it apart and then re-joining KHz (A69) with the codespace ID (e.g. 1303)
-  $codespacetag = $fields[2];
-  ($tech, $spce, $tagcode) = split(/-/, $codespacetag);
-  $codespace = join("-",$tech,$spce);
+  if ($ver == 2) {
+	$codespacetag = $fields[2];
+	($tech, $spce, $tagcode) = split(/-/, $codespacetag);
+	$codespace = join("-",$tech,$spce);
+  } else {
+	$codespace = $fields[1];
+	$tagcode = $fields[2];
+  }
 
   # BLOCK: Process data fields for sensor data (pressure, temp, acceler)
-  $data1 = $fields[5];
-  $units1 = $fields[6];
-  # Based on VEMCO e-mail exchange the other sensor should have a different timestamp, I'm not sure what differed in the era of VUE 1
-  $data2 = '';
-  $units2 = '';
-
+  if ($ver == 2) {
+	$data1 = $fields[5];
+	$units1 = $fields[6];
+	# Based on VEMCO e-mail exchange the other sensor should have a different timestamp, I'm not sure why it differed in the era of VUE 1
+	$data2 = '';
+	$units2 = '';
+  } else {
+	$data1 = $fields[3];
+	$units1 = $fields[4];
+	$data2 = $fields[5];
+	$units2 = $fields[6];
+  }
   # output the line to standard output, which is intercepted by the DOS .BAT file
   $outstring = join(",",($tagcode,$codespace,$outdate,$sernum,$data1,$units1,$data2,$units2));
   print "$outstring\n";
