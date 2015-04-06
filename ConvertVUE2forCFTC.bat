6796940135267cd2bbfbf0e6d4fe79fc84b51dfb
Matt Pagel <pagel@cs.wisc.edu>
2015-04-06 10:16:16 -0800

    cleanup
    cleanup of command-line switches /yesMS, /noMS, etc

--- ConvertVUE2forCFTC.bat
+++ ConvertVUE2forCFTC.bat
@@ -22,32 +22,32 @@ SET failures=0
 SET cmdXtra=""
 REM Check for any command-line parameters other than file names
 FOR %%x in (%*) do (
-   IF %%x=="/noMS" SET cmdXtra="/noMS"
-   IF %%x=="/yesMS" SET cmdXtra="/yesMS"
+   IF "%%x"=="/noMS" SET cmdXtra=/noMS
+   IF "%%x"=="/yesMS" SET cmdXtra=/yesMS
 )
+echo !cmdXtra!
 FOR %%x in (%*) do (
-   IF %%x=="/noMS" GOTO nxt
-   IF %%x=="/yesMS" GOTO nxt
-   SET /A argCount+=1
-   SET PF="%%~fx"
-   echo.
-   echo PROCESSING !argCount!: !EX! !cmdXtra! -from- !PF! -to- !FN!
-   REM Feel free to REM the two echo lines immediately above if you don't want to receive notification of each file processing
-   REM Supression may actually be handy if you are encountering a lot of file processing failures in order to see all those errors
-   CMD /C !EX! !cmdXtra! < !PF! >> !FN! 2>!EL!
-   FOR /F %%A IN ("!EL!") DO set size=%%~zA
-   IF !size! NEQ 0 (
-	echo Error message in !PF!: 1>&2
-	type "!EL!" 1>&2
-	echo. 1>&2
-	echo Common problem #1 Incorrect Perl path or missing Date::Calc library from Perl 1>&2
-	echo #2 XLS rather than CSV format or CSV with tabs as the delimiter between cols 1>&2
-	REM Feel free to REM the 3 echo statements immediately above if you don't want the same exact text each time
-	set /A failures+=1
-   )
+   echo %%x
+   IF "%%x" NEQ "/noMS" (
+      IF "%%x" NEQ "/yesMS" (
+         SET /A argCount+=1
+         SET PF="%%~fx"
+         echo.
+         echo PROCESSING !argCount!: !EX! !cmdXtra! -from- !PF! -to- !FN!
+REM Feel free to REM the two echo lines immediately above if you don't want to receive notification of each file processing
+REM Supression may actually be handy if you are encountering a lot of file processing failures in order to see all those errors
+         CMD /C !EX! !cmdXtra! < !PF! >> !FN! 2>!EL!
+         FOR /F %%A IN ("!EL!") DO set size=%%~zA
+         IF !size! NEQ 0 (
+            echo Error message in !PF!: 1>&2
+            type "!EL!" 1>&2
+            echo. 1>&2
+            echo Common problem #1 Incorrect Perl path or missing Date::Calc library from Perl 1>&2
+            echo #2 XLS rather than CSV format or CSV with tabs as the delimiter between cols 1>&2
+REM Feel free to REM the 3 echo statements immediately above if you don't want the same exact text each time
+            set /A failures+=1
+   )  )  )
    DEL !EL! > NUL
-:nxt
-   ECHO.
 )
 REM If not interested in having the window stick around after program execution, REM the next two lines
 ECHO Number of processed files: %argCount% (%failures% failures)
