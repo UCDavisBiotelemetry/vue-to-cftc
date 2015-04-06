7237208df6025ec0f7a7333a47580a0073740a3f
Matt Pagel <pagel@cs.wisc.edu>
2015-04-06 08:37:13 -0800

    modified:   VUE-to-cftc-plusMS.bat 	deleted:    ErrorWrapper.bat

--- VUE-to-cftc-plusMS.bat
+++ VUE-to-cftc-plusMS.bat
@@ -11,7 +11,7 @@ SET "HH=%dt:~8,2%" & set "Min=%dt:~10,2%" & set "Sec=%dt:~12,2%"
 SET EL2="%~dp0%EX%-error2-%YYYY%%MM%%DD%_%HH%%Min%%Sec%.log"
 SETLOCAL enabledelayedexpansion enableextensions
 
-CALL !EC! /noMS %* 2>!EL2!
+CALL !EC! /yesMS %* 2>!EL2!
 REM echo "!EL2! error log"
 FOR /F %%A IN ("!EL2!") DO ( 
 	set size=%%~zA
